2a114d1ee8b6b9f110a28249b99539a9d65b311f
Jorge Espinar <jor0veg@gmail.com>
2020-08-13 13:18:11 +0200

    Starting with email to developers
    Is neccesary a subscription. STOP until enough resources.

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout as do_logout, login as do_login
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import TemplateView
+from django import forms
 
 
 # Create your views here.
@@ -10,6 +12,41 @@ from django.urls import path
 from users import limpieza
 from users.limpieza import limpieza
 from users.vendor import productos, customers2, ventas
+
+class ContactForm(forms.Form):
+    name = forms.CharField(required=True)
+    from_email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
+
+
+
+
+def login(request):
+    # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=username, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('/products.html')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, "login.html", {'form': form})
+
 
 
 def welcome(request):
@@ -118,30 +155,6 @@ def reports(request):
 def register(request):
     return render(request, "register.html")
 
-def login(request):
-    # Creamos el formulario de autenticación vacío
-    form = AuthenticationForm()
-    if request.method == "POST":
-        # Añadimos los datos recibidos al formulario
-        form = AuthenticationForm(data=request.POST)
-        # Si el formulario es válido...
-        if form.is_valid():
-            # Recuperamos las credenciales validadas
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-
-            # Verificamos las credenciales del usuario
-            user = authenticate(username=username, password=password)
-
-            # Si existe un usuario con ese nombre y contraseña
-            if user is not None:
-                # Hacemos el login manualmente
-                do_login(request, user)
-                # Y le redireccionamos a la portada
-                return redirect('/products.html')
-
-    # Si llegamos al final renderizamos el formulario
-    return render(request, "login.html", {'form': form})
 
 def logout(request):
     # Finalizamos la sesión
