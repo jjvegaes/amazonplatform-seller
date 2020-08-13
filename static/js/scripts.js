2daaa7ece436f5baeb4eb88cfcca4e00981dab71
Jorge Espinar <jor0veg@gmail.com>
2020-08-13 11:09:40 +0200

    complexity
    corrections and complexness of the code

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -11,6 +11,20 @@ function closeNav() {
 }
 
 $(function () {
+        $("#from").datepicker({
+        onClose: function (selectedDate) {
+        $("#to").datepicker("option", "minDate", selectedDate);
+        }
+        });
+        $("#to").datepicker({
+        onClose: function (selectedDate) {
+        $("#from").datepicker("option", "maxDate", selectedDate);
+        }
+        });
+        });
+
+
+$(function () {
         $.datepicker.setDefaults($.datepicker.regional["es"]);
         $("#from").datepicker({
         minDate: "-20D",
